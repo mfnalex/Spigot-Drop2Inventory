b0ef6e27393786636c76be880d40a15071e6048b
mfnalex <1122571+mfnalex@users.noreply.github.com>
2018-11-08 12:55:45 +0100

    first commit

---
+++ src/de/jeffclan/Drop2Inventory/Drop2InventoryPlugin.java
@@ -0,0 +1,46 @@
+package de.jeffclan.Drop2Inventory;
+
+import java.util.HashMap;
+
+import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Drop2InventoryPlugin extends JavaPlugin implements Listener {
+	
+	public void onEnable() {
+		this.getServer().getPluginManager().registerEvents(this, this);
+	}
+	
+	@EventHandler
+	public void onBlockBreak(BlockBreakEvent event) {
+		
+		if(event.isCancelled()) {
+			return;
+		}
+		
+		Player player = event.getPlayer();
+		Block block = event.getBlock();
+		
+		if(player.getGameMode() == GameMode.CREATIVE) {
+			return;
+		}
+		
+		for ( ItemStack item : block.getDrops(player.getInventory().getItemInMainHand())) {
+			HashMap<Integer, ItemStack> leftOver = player.getInventory().addItem(item);
+			for(ItemStack leftoverItem : leftOver.values()) {
+				player.getWorld().dropItem(player.getLocation(),leftoverItem);
+			}
+		}
+		
+		event.setCancelled(true);
+		block.setType(Material.AIR);
+	}
+
+}
